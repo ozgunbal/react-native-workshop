be7d7a6be788a5b9f04dc3e3677d0b6507250c62
Ozgun Bal <ozgunbal@gmail.com>
2022-11-14 22:28:59 +0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,48 @@
+import React, { type PropsWithChildren } from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import Form from './Form';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: "white",
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+          {/* Import to type of custom Button from components/Button.tsx */}
+          {/* One should be primary and another one would be secondary */}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+});
+
+export default App;
---
+++ components/Button.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  View
+} from 'react-native';
+
+/* 
+2 type
+- Primary button: Background Red, font color white
+- Secondary Button: Background red, font color white, border red
+- prop: variant -> "Primary", "Secondary"
+- Use TouchableOpacity, View and Text
+- Optional: add disabled style with prop
+ */
+
+const Button = (props: any) => {
+  return null; // TODO
+
+}
+
+export default Button;
+
+
+const styles = StyleSheet.create({
+  primary: {},
+  secondary: {},
+});
