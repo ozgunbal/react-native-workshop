c5b498829eb41d15a302a0defb1b43db8328e248
Ozgun Bal <ozgunbal@gmail.com>
2022-11-15 14:14:06 +0300

    Remove unused variables

--- screens/Dashboard.tsx
+++ screens/Dashboard.tsx
@@ -1,21 +1,10 @@
 import React from 'react';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import { useContext } from 'react';
 import {
   Text,
   View,
 } from 'react-native';
-import { boolean } from 'yup';
 import { useUserData } from '../context/SessionContext';
 import TodoList from './TodoList';
-
-interface TodoType {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean
-}
 
 const Dashboard = () => {
   // Get session parameters(username, fullName) from SessionContext
